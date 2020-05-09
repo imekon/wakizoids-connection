d6cae9415f3e36c4e76126fc03b212119627890a
Pete Goodwin <imekon@users.noreply.github.com>
2020-05-09 09:35:01 +0100

    added initial resources

---
+++ scripts/PlayerShip.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+onready var firing_position = $FiringPosition
+onready var exhaust_position = $ExhaustPosition
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	pass
+	
---
+++ scripts/Welcome.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func on_start_pressed():
+	get_tree().change_scene("res://scenes/Main.tscn")
